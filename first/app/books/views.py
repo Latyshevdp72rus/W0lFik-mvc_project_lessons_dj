d655795893bce57bd74eac76645373983a4d439a
latyshevdp.contacts@yandex.ru <93967524+W0lFik@users.noreply.github.com>
2022-06-22 20:49:34 +0500

    second commit

---
+++ first/app/books/views.py
@@ -0,0 +1,8 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+
+def get_books_list(request):
+    m_book = Book.objects.get(pk=7)
+    return HttpResponse(f"<h1>{m_book}</h1>")
