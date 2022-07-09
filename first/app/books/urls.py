d75100528fc3523cc2f83925601efc8114a312d2
latyshevdp.contacts@yandex.ru <93967524+W0lFik@users.noreply.github.com>
2022-07-09 21:44:53 +0500

    DZ 02.07.2022 Создайте контроллер в котором выведите все данные о книге и связной с ним таблицей издательств

--- first/app/books/urls.py
+++ first/app/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
-from app.books.views import get_books_list, get_book
-
+from app.books.views import BooksDetail,BookList
 urlpatterns = [
-    path('', get_books_list),
-    path('<int:id_book>', get_book),
+    # path('', get_books_list),
+    path('', BookList.as_view()),
+    path('<int:pk>', BooksDetail.as_view()),
+    # path('<int:id_book>', get_book),
 
 ]
--- first/app/books/views.py
+++ first/app/books/views.py
@@ -1,77 +1,124 @@
 ﻿# from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from .models import Author, PublishingHouse, Book
+from .models import Book
 
 
-def get_books_list(request):
-    books = Book.objects.all()
-    context = {
-        'books': books,
-    }
-
-    return render(request, 'books/index.html', context)
+class BookList(ListView):
+    model = Book
+    context_object_name = 'books'
 
 
-def get_book(request, id_book):
-    books = Book.objects.get(pk=id_book)
-    return books
+class BooksDetail(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    pk_url_kwarg = 'pk'
 
-    # генерация шаблона
-    # books = Book.objects.all()
-    #
-    # context = {
-    #     'books': books,
-    #     'sale': True,}
 
-    # return HttpResponse(f"<h1>{gets_book}</h1>")
+# def get_books_list(request):
+#     books = Book.objects.all()
+#     context = {
+#         'books': books,
+#     }
+#
+#     return render(request, 'books/all_books.html', context)
 
-    # m_book = Book.objects.get()
-    # m_book = Book.objects.get(id_publishing_house__pub_house_name="Издательство 3")
-    # print(m_book.book_name)
-    # # m_book = Book.objects.all().first()
-    # pub_query = PublishingHouse.objects.all()
-    # author_query = Author.objects.filter(pk=1).prefetch_related('books')
-    # for i in pub_query:
-    #     pubg = i
-    #
-    # for j in author_query:
-    #     author = i
-    # books = author.book.first()
-    # print(books)
+# queryset = Book.objects.all()
+# template_name = 'book_list.html'
+# model = Book
+# context_object_name = 'books'
+# def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     context['books'] = self.queryset
+#     context['title'] = 'городская библиотека'
+#     return context
 
-    # author_query = Author.objects.filter(pk=1).prefetch_related('books')
-    # for a in author_query:
-    #     author = a
-    # context = {
-    #     "author": author,
-    # }
 
-    # pub_query = PublishingHouse.objects.prefetch_related('publishinghouse_books').all()
+# template_name = 'book_detail.html'
+# queryset = Book.objects.all()
+#
+# def get_object(self, pk):
+#     book = Book.objects.get(pk=pk)
+#     return book
+# template_name = 'books_detail.html'
+# print('ww')
 
-    # for pub_house in pub_query:
-    #     for book in pub_house.publishinghouse_books.all():
-    #         print(book.book_name)
-    #
 
-    # pub_query = PublishingHouse.objects.values(
-    #     'pub_house_name',
-    #     'pub_house_site'
-    # )
+# def get_book(request, id_book):
+#     books = Book.objects.get(pk=id_book)
+#     return books
 
-    # pub_query = PublishingHouse.objects.exclude(
-    #     is_daleted=False,
-    # )
-    # for pub_house in pub_query:
-    #     publisher = pub_house
-    #     print(publisher)
-    #
-    # return HttpResponse(f"<h1>{publisher}</h1>")
+#
+# def get_books_list(request):
+#     books = Book.objects.all()
+#     context = {
+#         'books': books,
+#     }
+#
+#     return render(request, 'books/all_books.html', context)
 
-    # author_list = (
-    #     Author(first_name="имя1",last_name='фам1' ,country='Россия' ,birthday="2002-01-02"),
-    #     Author(first_name="имя2", last_name='фам2', country='Россия', birthday="2002-01-02"),
-    #     )
-    # Author.objects.bulk_create(
-    #     author_list
-    # )
-    # return HttpResponse(f"<h1>ok</h1>")
+# генерация шаблона
+# books = Book.objects.all()
+#
+# context = {
+#     'books': books,
+#     'sale': True,}
+
+# return HttpResponse(f"<h1>{gets_book}</h1>")
+
+# m_book = Book.objects.get()
+# m_book = Book.objects.get(id_publishing_house__pub_house_name="Издательство 3")
+# print(m_book.book_name)
+# # m_book = Book.objects.all().first()
+# pub_query = PublishingHouse.objects.all()
+# author_query = Author.objects.filter(pk=1).prefetch_related('books')
+# for i in pub_query:
+#     pubg = i
+#
+# for j in author_query:
+#     author = i
+# books = author.book.first()
+# print(books)
+
+# author_query = Author.objects.filter(pk=1).prefetch_related('books')
+# for a in author_query:
+#     author = a
+# context = {
+#     "author": author,
+# }
+
+# pub_query = PublishingHouse.objects.prefetch_related('publishinghouse_books').all()
+
+# for pub_house in pub_query:
+#     for book in pub_house.publishinghouse_books.all():
+#         print(book.book_name)
+#
+
+# pub_query = PublishingHouse.objects.values(
+#     'pub_house_name',
+#     'pub_house_site'
+# )
+
+# pub_query = PublishingHouse.objects.exclude(
+#     is_daleted=False,
+# )
+# for pub_house in pub_query:
+#     publisher = pub_house
+#     print(publisher)
+#
+# return HttpResponse(f"<h1>{publisher}</h1>")
+
+# author_list = (
+#     Author(first_name="имя1",last_name='фам1' ,country='Россия' ,birthday="2002-01-02"),
+#     Author(first_name="имя2", last_name='фам2', country='Россия', birthday="2002-01-02"),
+#     )
+# Author.objects.bulk_create(
+#     author_list
+# )
+# return HttpResponse(f"<h1>ok</h1>")
+
+# def get_books_list(request):
+#     books = Book.objects.all()
+#     context = {
+#         'books': books,
+#     }
