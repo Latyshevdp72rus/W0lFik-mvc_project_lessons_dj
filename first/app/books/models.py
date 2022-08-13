99db3f64d5cca8d8629948f4e0867b1895114a94
latyshevdp.contacts@yandex.ru <93967524+W0lFik@users.noreply.github.com>
2022-08-13 14:21:39 +0500

    login and registration form in basic_registration and basic_login

--- first/app/books/models.py
+++ first/app/books/models.py
@@ -37,7 +37,7 @@ class Book(models.Model):
     )
     is_daleted = models.BooleanField(
         default=False,
-        verbose_name="Удален"
+        verbose_name="Удалить"
     )
     book_img = models.ImageField(
         upload_to='media/%y/%m/%d/',
@@ -55,13 +55,34 @@ class Book(models.Model):
 
 
 class PublishingHouse(models.Model):
-    pub_house_name = models.CharField(max_length=300, verbose_name="Название издательство")
-    pub_house_address = models.CharField(max_length=1500, verbose_name="Адрес издательства")
-    pub_house_contact_phone = models.CharField(max_length=20, verbose_name="Номер телефона издательства")
-    pub_house_email = models.EmailField(verbose_name="Название почты издательства")
-    pub_house_site = models.URLField(verbose_name="Сайт издательства", null=True, blank=True)
-    date_add = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
-    is_daleted = models.BooleanField(default=False, verbose_name="удалено")
+    pub_house_name = models.CharField(
+        max_length=300,
+        verbose_name="Название издательство"
+    )
+    pub_house_address = models.CharField(
+        max_length=1500,
+        verbose_name="Адрес издательства"
+    )
+    pub_house_contact_phone = models.CharField(
+        max_length=20,
+        verbose_name="Телефон издательства"
+    )
+    pub_house_email = models.EmailField(
+        verbose_name="Почта издательства"
+    )
+    pub_house_site = models.URLField(
+        verbose_name="Сайт издательства",
+        null=True,
+        blank=True
+    )
+    date_add = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата добавления"
+    )
+    is_daleted = models.BooleanField(
+        default=False,
+        verbose_name="Удалить"
+    )
 
     def __str__(self):
         return self.pub_house_name
@@ -72,20 +93,47 @@ class PublishingHouse(models.Model):
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=15, verbose_name="Имя")
-    last_name = models.CharField(max_length=15, verbose_name="Фамилия")
-    father_name = models.CharField(max_length=15, verbose_name="Отчество", null=True, blank=True)
-    country = models.CharField(max_length=30, verbose_name="Страна")
-    birthday = models.DateField(max_length=8, verbose_name="Дата рождения")
-    languages = models.JSONField(verbose_name='Язык на котором пишет автор', null=True, blank=True)
-    date_add = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
-    is_daleted = models.BooleanField(default=False, verbose_name="удалено")
+    first_name = models.CharField(
+        max_length=15,
+        verbose_name="Имя"
+    )
+    last_name = models.CharField(
+        max_length=15,
+        verbose_name="Фамилия"
+    )
+    father_name = models.CharField(
+        max_length=15,
+        verbose_name="Отчество",
+        null=True,
+        blank=True
+    )
+    country = models.CharField(
+        max_length=30,
+        verbose_name="Страна"
+    )
+    birthday = models.DateField(
+        max_length=8,
+        verbose_name="Дата рождения"
+    )
+    languages = models.JSONField(
+        verbose_name='Язык на котором пишет автор',
+        null=True,
+        blank=True
+    )
+    date_add = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата добавления"
+    )
+    is_daleted = models.BooleanField(
+        default=False,
+        verbose_name="Удалить"
+    )
 
     def __str__(self):
-        return self.last_name
+        return f'{self.last_name} {self.first_name} {self.father_name}'
 
     class Meta:
-        verbose_name = 'Автор'
+        verbose_name = 'Автора'
         verbose_name_plural = 'Авторы'
 
 
